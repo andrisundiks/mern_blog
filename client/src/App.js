03e7c7ec95e2fcec42af7cc5b7504002efa9fafe
n <not@provided.nil>
2019-05-09 15:56:44 +0100

    Added very basic structure for the blog. Decided to use Bulma because I hate css. There. I SAID IT.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Navbar from "./components/Navbar";
+import PostCard from "./components/PostCard";
 
 function App() {
   return (
-      <div></div>
+      <div className="is-ancestor">
+          <div className="container">
+              <Navbar/>
+              <div className="tile is-ancestor is-vertical">
+                  <PostCard />
+                  <PostCard />
+                  <PostCard />
+                  <PostCard />
+                  <PostCard />
+              </div>
+          </div>
+      </div>
   );
 }
 
